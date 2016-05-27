7dd8a09c3eddd6da1f5943c1d5dac6db307279dc
Naive Roboticist <naiveroboticist@gmail.com>
2016-05-27 18:26:47 -0400

    Cleaned up syntax and tested on v6.1.0.

--- test/pexecSpec.js
+++ test/pexecSpec.js
@@ -1,31 +1,29 @@
-var expect = require('chai').expect;
-var pexec = require('../lib/index.js');
+'use strict'
 
-describe("pexec", function() {
+const expect = require('chai').expect
+const pexec = require('../lib/index.js')
 
-  describe("#pexec", function() {
-
-    it("returns the correct current directory", function() {
-      pexec.pexec("pwd").
-        then(function(output) {
-          expect(output.stdout).to.equal(__directory);
-        }).
-        catch(function(err) {
+describe('pexec', function () {
+  describe('#pexec', function () {
+    it('returns the correct current directory', function () {
+      pexec.pexec('pwd')
+        .then(function (output) {
+          expect(output.stdout).to.equal(__directory)
+        })
+        .catch(function (err) {
           expect(err).to.be
-        });
-    });
+        })
+    })
 
-    it("returns an error if a non-existent command is executed", function() {
-      pexec.pexec("nonexistent_command").
-        then(function(output) {
-          expect(output).to.equal(null);
-        }).
-        catch(function(err) {
-          expect(err.killed).to.equal(false);
-          expect(err.code).to.equal(127);
-        });
-    });
-
-  });
-
-});
+    it('returns an error if a non-existent command is executed', function () {
+      pexec.pexec('nonexistent_command')
+        .then(function (output) {
+          expect(output).to.equal(null)
+        })
+        .catch(function (err) {
+          expect(err.killed).to.equal(false)
+          expect(err.code).to.equal(127)
+        })
+    })
+  })
+})
